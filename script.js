c1c3c42c1704a44bb42529d9aaa7e5d08d7053ee
Sanvi Singh <100112668+sanvi1803@users.noreply.github.com>
2024-05-09 14:35:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,93 @@
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+
+let taskCount = 0;
+
+const displayCount = (taskCount) => {
+    countValue.innerHTML = taskCount;
+};
+
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+    error.style.display = "none";
+    if (!taskName) {
+        setTimeout(() => {
+            error.style.display = "block";
+        }, 200);
+        return;
+    }
+
+    const task = `<div class="task">
+        <input type="checkbox" class="task-check">
+            <span class="taskname">${taskName}</span>
+        <button class="edit">
+            <i class="fa-solid fa-pen-to-square"></i>  
+        </button>
+        <button class="delete">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    </div>`;
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach((button) => {
+        button.onclick = () => {
+            button.parentNode.remove();
+            taskCount -= 1;
+            displayCount(taskCount);
+        }
+    });
+
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach((editBtn) => {
+        editBtn.onclick = (e) => {
+            let targetElement = e.target;
+            if (!(e.target.className == "edit")) {
+                targetElement = e.target.parentElement;
+            }
+            newTaskInput.value = targetElement.previousElementSibling?.innerText;
+            targetElement.parentNode.remove();
+            taskCount-=1;
+            displayCount(taskCount);
+        };
+    })
+
+    const tasksCheck = document.querySelectorAll(".task-check");
+    tasksCheck.forEach((checkbox) => {
+        checkbox.onchange = () =>{
+            checkbox.nextElementSibling.classList.toggle("completed");
+            if(checkbox.checked){
+                taskCount-=1;
+            }
+            else{
+                taskCount+=1;
+            }
+            displayCount(taskCount);
+        }
+    })
+    taskCount+=1;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+};
+
+// The below function lets you enter task even on by clicking enter key rather than clicking on add button
+// const onEnterKeyPress = (event) => {
+//     if (event.key === "Enter") {
+//         addTask();
+//     }
+// };
+
+// newTaskInput.addEventListener("keypress", onEnterKeyPress);
+
+
+addBtn.addEventListener("click", addTask);
+
+window.onload =() =>{
+    taskCount=0;
+    displayCount(taskCount);
+    newTaskInput.value = "";
+}
